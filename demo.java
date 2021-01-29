1f51b3f4713f5404d72869c80fd8d512c6b05dcb
Kevin <kevinsuwandi6@gmail.com>
2021-01-29 15:36:14 +0700

    First Commit by Kevin

---
+++ demo.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Biodata {
+
+    public static void main(String[] args) {
+  
+      
+      Scanner input_bio=new Scanner (System.in);
+        System.out.println("Biodata Siswa CCIT yang ikut seminar");
+        System.out.println("----------------");
+     
+      
+        
+      
+      System.out.println("Nama = Kevin Suwandi");
+      System.out.println("Nim = 1920010112");
+        System.out.println("Kelas = 3SC2");
+        System.out.println("Jurusan = Software Enginering ");
+        System.out.println("\n");
+        System.out.println("--- Print Biodata seminar----");
+    }
+    }
+
+//testingDEMO
